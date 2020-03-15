608911615631e44d0b02681e6e570e0824fba1b9
moussa Doumbia <moussa.doumbia9219@gmail.com>
2020-03-15 22:04:30 +0100

    making prisma working with graphql yoga

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,25 @@
-const Mutations = {};
+const mutation = {
+   async createItem(parent, args, ctx, info) {
+        // TODO: check if they are logged in
 
-module.exports = Mutations;
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        console.log(item);
+
+        return item;
+    }
+    // createDog(parent, args, ctx, info){
+    //     global.dogs = global.dogs || []
+    //     // create a dog
+    //     const newDog =  {name: args.name}
+    //     global.dogs.push(newDog);
+    //     return newDog;
+    // }
+
+};
+
+module.exports = mutation;
