9e6c90795d87b07cc62cffb9724341f12bae72dd
moussa Doumbia <moussa.doumbia9219@gmail.com>
2020-03-16 09:51:43 +0100

    setup apollo client for frontent

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,31 @@
-import App, {Container} from 'next/app';
-import Page from "../components/Page"
-class MyApp extends App {
-    render() {
-        const {Component} = this.props;
+import App, { Container } from "next/app";
+import Page from "../components/Page";
+import { ApolloProvider } from "react-apollo";
+import withData from "../lib/withData";
 
-        return(
-            <Container>
-                <Page>
-                    <Component />
-                </Page>
-            </Container>
-        )
-    }
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+		let pageProps = {};
+		if(Component.getInitialProps) {
+			pageProps = await Component.getInitialProps(ctx);
+		}
+		// this expose the query to the user
+		pageProps.query = ctx.query;
+		return { pageProps };
+	}
+  render() {
+    const { Component, apollo, pageProps } = this.props;
+
+    return (
+      <Container>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
+      </Container>
+    );
+  }
 }
 
-export default MyApp;
+export default withData(MyApp);
